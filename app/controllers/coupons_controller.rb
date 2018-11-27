6945c60b90413d5274a64a4810e3ea1530612fa8
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-11-27 11:17:15 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,28 @@
+class CouponsController < ApplicationController
+
+def index
+@coupons = Coupon.all
+
+render 'index'
+end
+
+def show
+
+  @coupon = Coupon.find(params[:id])
+
+
+end
+
+def new
+
+end
+
+def create
+
+  @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+
+  redirect_to coupons_path(@coupon)
+end
+
+end
+end
